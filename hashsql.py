dd0f329f48a0ad83c337103acd3a1f365c0af4db
Collin Paul <cspaul@protonmail.com>
2021-06-29 12:47:20 -0400

    added exception handler and sql insert

--- hashsql.py
+++ hashsql.py
@@ -10,32 +10,49 @@ if len(sys.argv) != 4:
     print("Usage: ./hashsql.py <username> <password> <database>")
     sys.exit(1)
 
-dbConnect = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+def exception_handler(x):
+    template = "An exception of type {0} occured. Arguements:\n{1!r}"
+    message = template.format(type(x).__name__, x.args)
+    print(message)
+    exit(1)
 
-cursor = dbConnect.cursor()
+try:
+    dbConnect = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = dbConnect.cursor()
+except Exception as ex:
+    exception_handler(ex)
 
 def csv_to_list():
+    #Takes a csv file as input, returns the data as a list
     print("enter file name: ")
     file_name = input()
 
-    with open(file_name, newline='') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-    f.close()
-
+    try:
+        with open(file_name, newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+        f.close()
+    except Exception as ex:
+        exception_handler(ex)
+    
     return data
 
 def generate_hash():
+    #Create hashes for data in the second field of each list item
+    #Upload hashes along with an ID to mysql.
     data = csv_to_list()
-
-    for item in data:
-        ID = item[0]
-        to_hash = item[1]
-        x = bcrypt.hashpw(to_hash.encode(), bcrypt.gensalt())
-
-        
-        #print(ID)
-        #print(x)
+    sql = "INSERT INTO users (email, password) VALUES (%s, %s)"
+    try:
+        for item in data:
+            ID = item[0]
+            to_hash = item[1]
+            x = bcrypt.hashpw(to_hash.encode(), bcrypt.gensalt())
+            val = (ID, x.decode())
+            cursor.execute(sql, val)
+            dbConnect.commit()
+            print(cursor.rowcount, "record inserted")
+    except Exception as ex:
+        exception_handler(ex)
 
 def check_hash():
     #user enters UserID and password string, query database, pull password hash
@@ -48,10 +65,7 @@ def check_hash():
     myHash = f.read()
     myHash = myHash.rstrip('\n')
     myHash = myHash.encode()
-
-    #print(myHash)
     
-
     if bcrypt.checkpw(password, myHash):
         print('access granted')
         sql_connect(password.decode())
@@ -60,5 +74,3 @@ def check_hash():
  
 
 generate_hash()
-
-
